bda396c4143e45b72042ee390fd3ceea89219a24
Alistair Cooke <alcooke@Alistairs-MacBook-Air.local>
2022-10-19 19:50:49 +0100

    extensions completed

--- w1d3_homework.py
+++ w1d3_homework.py
@@ -11,27 +11,27 @@ uncompleted_tasks = []
 for task in tasks:
     if task["completed"] == False:
         uncompleted_tasks.append(task)
-#print(uncompleted_tasks)
+print(uncompleted_tasks)
 
 #Print a list of completed tasks
 completed_tasks = []
 for task in tasks:
     if task["completed"] == True:
         completed_tasks.append(task)
-#print(completed_tasks)
+print(completed_tasks)
 
 #Print a list of all task descriptions
 task_description = []
 for task in tasks:
     task_description.append(task["description"])
-#print(task_description)
+print(task_description)
 
 #Print a list of tasks where time_taken is at least a given time
 long_tasks = []
 for task in tasks:
     if task["time_taken"] > 20:
         long_tasks.append(task)
-#print(long_tasks)
+print(long_tasks)
 
 #Print any task with a given description
 def find_task(list, description):   
@@ -40,8 +40,18 @@ def find_task(list, description):
         if task["description"] == description:
             given_description = task
     return given_description
-#print(find_task(tasks, "Feed Cat"))
+print(find_task(tasks, "Feed Cat"))
 
 #Given a description update that task to mark it as complete.
-
+def update_task(list, description):
+    completed_task = None
+    for task in list:
+        if task["description"] == description:
+            task["completed"] = True
+            completed_task = task
+    return completed_task
+print(update_task(tasks, "Wash Dishes"))
+    
 #Add a task to the list
+tasks.append({"description": "Mop Floor", "completed": True, "time_taken": 30})
+#print(tasks)
